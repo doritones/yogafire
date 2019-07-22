6365dbe2babb0d9dde809a6fe94bc11f114c0a64
Denis Araki <denisaraki@gmail.com>
2019-07-22 19:59:14 +0200

    All fake content inserted

--- src/components/Layout/Price.js
+++ src/components/Layout/Price.js
@@ -3,7 +3,43 @@ import './Layout.css';
 
 const Price = () => (
     <div className='Price'>
-        <p className="PriceText">Price</p>
+        <div className='PriceContent'>
+            <h3 className="PriceText">Preço e pagamento</h3>
+                <div className='PriceDivide'>
+                    <div className=' split PriceTable'>
+                        <table className='Table'>
+                            <thead>
+                                <tr>
+                                    <th colspan='2'>Valores</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>1 hora</td>
+                                    <td>R$ 100,00 / hr</td>
+                                </tr>
+                                <tr>
+                                    <td>2 horas ou +</td>
+                                    <td>R$ 90,00 / hr</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                    <div className=' split PriceTextContent'>
+                        <p>Suspendisse sagittis nibh nec sem ullamcorper, ac porttitor leo ornare. Phasellus gravida posuere 
+                        consequat. Donec id viverra quam. Curabitur dui augue, aliquam eget sem at, sagittis condimentum augue. 
+                        Suspendisse feugiat nunc eget augue bibendum rutrum. Fusce congue sem ac libero varius, quis sollicitudin 
+                        enim accumsan. Vivamus tincidunt dui et magna elementum venenatis. Vivamus in iaculis odio. Ut mattis 
+                        vulputate neque, ut dictum lacus venenatis porttitor.</p>
+
+                        <p>In rutrum ex eget consectetur cursus. Etiam rhoncus convallis orci at vehicula. Duis venenatis sodales 
+                        risus, vel consequat lorem mollis nec. Suspendisse vel leo hendrerit felis aliquet dignissim vel nec 
+                        magna. Sed non mollis eros, id pulvinar purus. Phasellus ullamcorper gravida turpis, sed cursus ex 
+                        tincidunt id. Duis tellus enim, pharetra non iaculis eget, euismod sollicitudin massa. Etiam vel 
+                        congue est. Fusce et volutpat ex.</p>
+                    </div>
+                </div>
+        </div>
     </div>
 )
 
--- src/components/Layout/Teacher.js
+++ src/components/Layout/Teacher.js
@@ -3,7 +3,22 @@ import './Layout.css';
 
 const Teacher = () => (
     <div className='Teacher'>
-        <p className="TeacherText">Teacher</p>
+        <div className='TeacherContent'>
+            <div className='split TeacherText'>
+                <div className='TeacherTextContent'>
+                    <div className='Bottom'>
+                        <h3>Professor Dhalsim</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur lorem nibh, 
+                            volutpat nec tincidunt eu, pellentesque in est. Proin in nibh sed mauris sollicitudin aliquet 
+                            laoreet a erat. Integer justo mauris, interdum a tempus imperdiet, tristique suscipit urna. 
+                            Mauris massa mauris, cursus et ligula non, varius lobortis sem. Etiam malesuada convallis ante, vitae 
+                            cursus velit sollicitudin commodo. Nunc molestie accumsan elit, eu viverra quam commodo non. 
+                            Nam ut diam ullamcorper justo finibus condimentum.</p>
+                    </div>
+                </div>
+            </div>
+            <div className='split TeacherImage'></div>
+        </div>
     </div>
 )
 
